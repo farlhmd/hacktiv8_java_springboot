bd9b1e2426043e5967b7326300305df86a01f33b
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-14 20:44:12 +0700

    Create TipeData.java

---
+++ day_1/TipeData.java
@@ -0,0 +1,12 @@
+package day_1;
+
+public class TipeData {
+    public static void main(String[] args) {
+        int duaPuluh = 20;
+        String sepuluh = "10";
+        System.out.println("Angka pertama: "+duaPuluh);
+        System.out.println("Angka kedua: "+Integer.parseInt(sepuluh));
+        
+
+    }
+}
