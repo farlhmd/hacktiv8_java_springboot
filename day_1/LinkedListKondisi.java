e749222f31db9ebd2f6c8b032951565b4b1dcab3
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-14 22:19:51 +0700

    Create LinkedListKondisi.java

---
+++ day_1/LinkedListKondisi.java
@@ -0,0 +1,29 @@
+package day_1;
+
+import java.util.LinkedList;
+
+
+public class LinkedListKondisi {
+    public static void main(String[] args) {
+        LinkedList<String> mobil = new LinkedList<>();
+        mobil.add("BMW");
+        mobil.add("Mercedes");
+        mobil.add("Volvo");
+        mobil.add("Mazda");
+        mobil.add("Honda");
+
+        //System.out.println(mobil);
+
+        if (mobil.isEmpty()) {
+            System.out.println("List mobil kosong");
+        }else{
+            System.out.println("Data mobil terisi, total data mobil: " + mobil.size() + ".\n");
+        }
+
+        if (mobil.contains("Honda")) {
+            System.out.println("Mobil yang anda cari \'Honda' ditemukan.");
+        }else{
+            System.out.println("Mobil yang anda cari tidak ditemukan.");
+        }
+    }
+}
