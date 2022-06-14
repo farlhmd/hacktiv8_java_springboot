3533bee2dffe10002e57c55784f938a94694e5d7
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-14 21:11:16 +0700

    Update ListJava.java

--- day_1/ListJava.java
+++ day_1/ListJava.java
@@ -18,9 +18,9 @@ public class ListJava{
         listPertama.add("Mazda");
         listPertama.add("Honda");
         //show list pertama
-        System.out.println("List pertama (format ArrayList) merupakan kumpulan merk mobil:\n" + listPertama);
+        System.out.println("\nList pertama (format ArrayList) merupakan kumpulan merk mobil:\n" + listPertama);
         //akses bagian dari list
-        System.out.println("Urutan list ke 3: "+listPertama.get(2));
+        System.out.println("Urutan list ke 3: "+listPertama.get(2) + "\n");
 
 
         //add list kedua
@@ -29,18 +29,18 @@ public class ListJava{
         listKedua.add("Ubuntu");
         listKedua.add("iOS");
         listKedua.add("Android");
-        System.out.println("List kedua (format LinkedList) merupakan kumpulan jenis Operating System:\n" + listKedua);
+        System.out.println("List kedua (format LinkedList) merupakan kumpulan jenis Operating System:\n" + listKedua + "\n");
 
         //function dari LinkedList
         listKedua.addFirst("Unix");
         listKedua.addLast("Manjaro");
 
-        System.out.println("Menambahkan data pada awal dan akhir dari list kedua:\n" + listKedua);
+        System.out.println("Menambahkan data pada awal dan akhir dari list kedua:\n" + listKedua + "\n");
 
         listKedua.removeFirst();
         listKedua.removeLast();
 
-        System.out.println("Menghapus data awal dan akhir dari list kedua:\n" + listKedua);
+        System.out.println("Menghapus data awal dan akhir dari list kedua:\n" + listKedua + "\n");
 
 
     }
