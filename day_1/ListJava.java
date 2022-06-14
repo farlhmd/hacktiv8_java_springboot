20950b488e97ebffb7e4998cea40de4bcdd519da
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-14 21:09:14 +0700

    Update ListJava.java

--- day_1/ListJava.java
+++ day_1/ListJava.java
@@ -2,25 +2,46 @@ package day_1;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 
 public class ListJava{
     public static void main(String[] args) {
-        List<String> listPertama = new ArrayList<>();
-        List<String> listKedua = new LinkedList<>();
+        //When To Use
+        //Use an ArrayList for storing and accessing data, and LinkedList to manipulate data.
+
+        ArrayList<String> listPertama = new ArrayList<>();
+        LinkedList<String> listKedua = new LinkedList<>();
 
         //add list pertama
         listPertama.add("BMW");
-        listPertama.add("Mercedes Benz");
+        listPertama.add("Mercedes");
         listPertama.add("Volvo");
         listPertama.add("Mazda");
         listPertama.add("Honda");
         //show list pertama
-        System.out.println("List pertama merupakan kumpulan merk mobil:\n"+listPertama);
+        System.out.println("List pertama (format ArrayList) merupakan kumpulan merk mobil:\n" + listPertama);
+        //akses bagian dari list
+        System.out.println("Urutan list ke 3: "+listPertama.get(2));
 
-        
 
         //add list kedua
-        
+        listKedua.add("MacOs");
+        listKedua.add("Windows");
+        listKedua.add("Ubuntu");
+        listKedua.add("iOS");
+        listKedua.add("Android");
+        System.out.println("List kedua (format LinkedList) merupakan kumpulan jenis Operating System:\n" + listKedua);
+
+        //function dari LinkedList
+        listKedua.addFirst("Unix");
+        listKedua.addLast("Manjaro");
+
+        System.out.println("Menambahkan data pada awal dan akhir dari list kedua:\n" + listKedua);
+
+        listKedua.removeFirst();
+        listKedua.removeLast();
+
+        System.out.println("Menghapus data awal dan akhir dari list kedua:\n" + listKedua);
+
+
     }
 }
