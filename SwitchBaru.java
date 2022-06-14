6f415363f9d5ffcf9f86ef9e8ad5f2863123705c
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-14 13:06:05 +0700

    add Switch

---
+++ SwitchBaru.java
@@ -0,0 +1,19 @@
+public class SwitchBaru {
+    public static void main(String[] args) {
+
+        String hari = "Kamis";
+        String hasil = switch (hari) {
+            case "Senin", "Selasa", "Rabu", "Kamis" -> "Weekday";
+            case "Sabtu", "Minggu" -> "Weekend";
+            default ->
+            {
+                if(hari.isEmpty())
+                hasil = "Masukkan hari dengan benar";
+                else
+                hasil = "Sepertinya hari Jum'at";
+                throw new IllegalStateException("Invalid day: "+ hasil);
+            }
+        };
+        System.out.println(hasil);
+    }
+}
---
+++ SwitchLama.java
@@ -0,0 +1,31 @@
+/**
+ * switchLama
+ */
+public class SwitchLama{
+
+    public static void main(String[] args) {
+
+        String hari = "Jum'at";
+        String hasil = "";
+
+        switch (hari) {
+            case "Senin":
+            case "Selasa":
+            case "Rabu":
+            case "Kamis":
+            
+            case "Jum'at": {
+                hasil = "Hari ini harus shalat Jum'at ";
+                break;
+            }
+
+            case "Sabtu", "Minggu":{
+                hasil = "Asikk liburann";
+                break;
+            }
+            default:
+                break;
+        }
+        System.out.println(hasil);
+    }
+}
