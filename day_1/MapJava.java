63686b34b7cc26a4bc3d349bfcc6e34bd027b24d
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-14 22:19:56 +0700

    Create MapJava.java

---
+++ day_1/MapJava.java
@@ -0,0 +1,21 @@
+package day_1;
+
+import java.util.HashMap;
+
+public class MapJava {
+    public static void main(String[] args) {
+        HashMap<String, String> ibuKota = new HashMap<String, String>();
+
+        ibuKota.put("Inggris", "London");
+        ibuKota.put("Jerman", "Berlin");
+        ibuKota.put("Indonesia", "Jakarta");
+        ibuKota.put("USA", "Washington DC");
+
+        System.out.println("Ibukota negara: (format Negara=Ibu Kota)\n" + ibuKota);
+
+        //mengakses data dengan 'key'
+
+        System.out.println("Ibu Kota negara Jerman adalah " + ibuKota.get("Jerman"));
+    }
+    
+}
