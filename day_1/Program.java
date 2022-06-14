125c0f88113f7fa9d8a2ce5934a5f7818a8bae97
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-14 20:44:07 +0700

    Create Program.java

---
+++ day_1/Program.java
@@ -0,0 +1,17 @@
+package day_1;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("\nHenlo Java!");
+        System.out.println("Henlo Hacktiv8!\n");
+
+        if (true) {
+            System.out.println("ini jika nilai if pertama \'true' \n");
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Perulangan ke "+i);
+        }
+    }
+    
+}
