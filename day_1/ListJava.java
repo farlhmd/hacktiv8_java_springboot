3755c067a76124a77f7702f929117c8d1f54a36f
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-14 20:44:05 +0700

    Create ListJava.java

---
+++ day_1/ListJava.java
@@ -0,0 +1,26 @@
+package day_1;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListJava{
+    public static void main(String[] args) {
+        List<String> listPertama = new ArrayList<>();
+        List<String> listKedua = new LinkedList<>();
+
+        //add list pertama
+        listPertama.add("BMW");
+        listPertama.add("Mercedes Benz");
+        listPertama.add("Volvo");
+        listPertama.add("Mazda");
+        listPertama.add("Honda");
+        //show list pertama
+        System.out.println("List pertama merupakan kumpulan merk mobil:\n"+listPertama);
+
+        
+
+        //add list kedua
+        
+    }
+}
