91f75de220c0b2d178529c259522e4681803cc52
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-15 13:36:28 +0700

    Update OperatorAritmatika.java

--- sesi2/OperatorAritmatika.java
+++ sesi2/OperatorAritmatika.java
@@ -5,11 +5,52 @@ import java.util.Scanner;
 public class OperatorAritmatika {
     public static void main(String[] args) {
         int angka1, angka2, hasil;
+        String operatorDigunakan, pilihanOperator;
 
-        Scanner inputanUser = new Scanner(System.in);
+        Scanner inputanAngka = new Scanner(System.in);
+        Scanner inputanOperator = new Scanner(System.in);
 
         System.out.println("Input angka pertama: ");
-        angka1 = inputanUser.nextInt();
+        angka1 = inputanAngka.nextInt();
+        System.out.println("Input angka kedua: ");
+        angka2 = inputanAngka.nextInt();
+
+        System.out.println("Silahkan pilih Operator:");
+        System.out.println("a. Penjumlahan");
+        System.out.println("b. Pengurangan");
+        System.out.println("c. Perkalian");
+        System.out.println("d. Pembagian");
+        pilihanOperator = inputanOperator.nextLine();
+
+        switch (pilihanOperator) {
+            case "a", "A":
+            hasil = angka1 + angka2;
+            operatorDigunakan = "penjumlahan";
+            System.out.println("Hasil " + operatorDigunakan + " adalah " + hasil);
+            break;
+            case "b", "B":
+            hasil = angka1 - angka2;
+            operatorDigunakan = "pengurangan";
+            System.out.println("Hasil " + operatorDigunakan + " adalah " + hasil);
+            break;
+            case "c", "C":
+            hasil = angka1 * angka2;
+            operatorDigunakan = "perkalian";
+            System.out.println("Hasil " + operatorDigunakan + " adalah " + hasil);
+            break;
+            case "d", "D":
+            hasil = angka1 / angka2;
+            operatorDigunakan = "pembagian";
+            System.out.println("Hasil " + operatorDigunakan + " adalah " + hasil);
+            break;
+        
+            default:
+            System.out.println("Inputan salah. Hanya a,b,c,d. Silahkan ulangi kembali.");
+        }
+
+        
+       
+        
         
         
     }
