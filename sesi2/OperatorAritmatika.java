cb46d8c33683440ff7a1b51099e97d4a068f69a7
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-15 10:31:20 +0700

    Update OperatorAritmatika.java

--- sesi2/OperatorAritmatika.java
+++ sesi2/OperatorAritmatika.java
@@ -7,6 +7,10 @@ public class OperatorAritmatika {
         int angka1, angka2, hasil;
 
         Scanner inputanUser = new Scanner(System.in);
+
+        System.out.println("Input angka pertama: ");
+        angka1 = inputanUser.nextInt();
+        
         
     }
 }
