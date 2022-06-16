e8ee5e55aeaafcfa53715e41db242d02f55f288c
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-16 15:03:29 +0700

    add sesi 5

---
+++ sesi5/ClassMobil.java
@@ -0,0 +1,23 @@
+package sesi5;
+
+public class ClassMobil {
+   public static void main(String[] args) {
+      var merk = "";
+      var manufaktur = "";
+      var mesin = "";
+
+      pintuDibuka();
+      mesinDiGas();
+      remDitekan();
+
+   }
+
+   private static void remDitekan() {
+   }
+
+   private static void mesinDiGas() {
+   }
+
+   private static void pintuDibuka() {
+   }
+}
---
+++ sesi5/SegitigaSiku.java
@@ -0,0 +1,22 @@
+package sesi5;
+
+public class SegitigaSiku extends BangunDatar {
+    float alas, tinggi;
+    float sisiMiring;
+
+    @Override
+    float luas() {
+        float luas = alas * tinggi;
+        System.out.println("Luas segitiga siku-siku adalah      : " + luas);
+        return luas;
+    }
+
+    @Override
+    float keliling() {
+        float sisiMiring = (float) Math.sqrt(Math.pow(alas, 2) + Math.pow(tinggi, 2));
+        float kll = alas + tinggi + sisiMiring;
+        System.out.println("Keliling segitiga siku-siku adalah  : " + kll + "\n");
+        return kll;
+    }
+
+}
