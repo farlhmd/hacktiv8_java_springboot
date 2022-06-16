eee07d9cb54e5594fc21e34feef5e9c72cce5d1d
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-16 22:38:54 +0700

    Update Cetak.java

--- sesi5/Cetak.java
+++ sesi5/Cetak.java
@@ -8,9 +8,13 @@ public class Cetak {
             return b;
         }
     }
+    static int maxNumber(int a, int b){
+        if (a>b) return a;
+        else return b;
+    }
 
     public static void main(String[] args) {
         System.out.println(maxNumber(10, 20));
-        // System.out.println(4,5,7,5);
+        //System.out.print(maxNumber(4,5,7,5));
     }
 }
