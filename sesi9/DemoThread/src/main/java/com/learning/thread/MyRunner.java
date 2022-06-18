505b7b2b8c1c132314fcfc2a9e79846da3f786e5
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-18 17:46:15 +0700

    Update MyRunner.java

--- sesi9/DemoThread/src/main/java/com/learning/thread/MyRunner.java
+++ sesi9/DemoThread/src/main/java/com/learning/thread/MyRunner.java
@@ -30,7 +30,7 @@ public class MyRunner {
     public List<User> parseUsers() {
 
         MyHandler handler = new MyHandler();
-        String fileName = "src/res/Users.xml";
+        String fileName = "src/res/users.xml";
         File xmlDocument = Paths.get(fileName).toFile();
 
         try {
