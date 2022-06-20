d380ee51126365920276ca802d5062889ecb734f
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-20 09:34:49 +0700

    add DemoAlexa

---
+++ sesi9/DemoAlexa/src/main/java/com/learning/alexa/DataProcessor.java
@@ -0,0 +1,14 @@
+package com.learning.alexa;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+
+public class DataProcessor {
+	public static InputStream getData(URLConnection conn) throws IOException {
+		
+		// InputStream -> (stream of bytes) that reads from this connection
+		InputStream is = conn.getInputStream();
+		return is;
+	}
+}
