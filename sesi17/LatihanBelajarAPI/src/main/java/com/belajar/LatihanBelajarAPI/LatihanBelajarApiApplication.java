f9d78080148f1d9aad9183bebec271398acfb429
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-23 21:40:24 +0700

    update sesi 17

---
+++ sesi17/LatihanBelajarAPI/src/main/java/com/belajar/LatihanBelajarAPI/LatihanBelajarApiApplication.java
@@ -0,0 +1,13 @@
+package com.belajar.LatihanBelajarAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihanBelajarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatihanBelajarApiApplication.class, args);
+	}
+
+}
