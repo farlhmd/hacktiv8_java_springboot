3259f3aa3023b05f5cb1dea12d11a50b5f8dbb81
farlhmd <57334075+farlhmd@users.noreply.github.com>
2022-06-24 09:27:59 +0700

    add sesi 17

--- sesi17/LatihanBelajarAPI/src/main/java/com/belajar/LatihanBelajarAPI/model/User.java
+++ sesi17/LatihanBelajarAPI/src/main/java/com/belajar/LatihanBelajarAPI/model/User.java
@@ -1,32 +1,50 @@
 package com.belajar.LatihanBelajarAPI.model;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
-@Entity
-@Table(name = "users")
-public class User{
-    private int id;
-    private String firstName;
-    private String lastName;
+public class User {
+	private int id;
+	private String firstName;
+	private String lastName;
+	
+	public User() {
+		
+	}
+	
+	public User(int id, String firstName, String lastName) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int getId() {
+		return id;
+	}
 
-    public User(){
+	public String getFirstName() {
+		return firstName;
+	}
 
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public User(int id, String firstName, String lastName){
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	public String getLastName() {
+		return lastName;
+	}
 
-    public int getId(){
-        return id;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
 }
 
